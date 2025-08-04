0c09d5252dc74b5f745e653a1d6c9344a13d5891
Nicolas Peña <nicolaspenai2010@gmail.com>
2025-08-04 17:16:00 -0500

    Finalizacion de la Class Note.

--- src/notebook.py
+++ src/notebook.py
@@ -1,9 +1,24 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
-
+from datetime import datetime
 class Note:
     HIGH : str = "HIGH"
     MEDIUM : str = "MEDIUM"
     LOW: str = "LOW"
+
+    def __init__(self,code:str,title:str,text:str,importance:str,create_datetime: datetime, tags : list):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.create_datetime = datetime.now()
+        self.tags = list[str]
+
+    def add_tag(self,tag:str):
+        self.tags.append(tag)
+
+    def __str__(self)->str:
+        return f"Date: {self.create_datetime} , {self.title}: {self.text}"
+
 
 
 
