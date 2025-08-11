8dfd3fda29e1eb0bae14eae450f05537ea9227aa
Nicolas Peña <nicolaspenai2010@gmail.com>
2025-08-11 17:01:16 -0500

    Proceso de la Class Notebook

--- src/notebook.py
+++ src/notebook.py
@@ -8,16 +8,17 @@ class Note:
     MEDIUM : str = "MEDIUM"
     LOW: str = "LOW"
 
-    def __init__(self,code:int,title:str,text:str,importance:str,create_datetime: datetime, tags : list):
+    def __init__(self,code:int,title:str,text:str,importance:str):
         self.code = code
         self.title = title
         self.text = text
         self.importance = importance
-        self.create_datetime = datetime.now()
-        self.tags = list[str]
+        self.create_datetime: datetime = datetime.now()
+        self.tags = list[str] = [ ]
 
     def add_tag(self,tag:str):
-        self.tags.append(tag)
+        if tag not in self.tags:
+            self.tags.append(tag)
 
     def __str__(self)->str:
         return f"Date: {self.create_datetime} , {self.title}: {self.text}"
